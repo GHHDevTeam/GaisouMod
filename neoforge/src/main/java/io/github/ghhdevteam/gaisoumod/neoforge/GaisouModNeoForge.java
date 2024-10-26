5d82411241ae0e010de74b8cd44d37a4b31b11e9
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-10-26 14:48:05 +0900

    もでぃど

--- neoforge/src/main/java/io/github/ghhdevteam/gaisoumod/neoforge/GaisouModNeoForge.java
+++ neoforge/src/main/java/io/github/ghhdevteam/gaisoumod/neoforge/GaisouModNeoForge.java
@@ -5,7 +5,7 @@ import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import io.github.ghhdevteam.gaisoumod.mpl.MPLGaisouMod;
 
-@Mod(MPLGaisouMod.MOD_ID)
+@Mod(MPLGaisouMod.もでぃど)
 public class GaisouModNeoForge {
     public GaisouModNeoForge(ModContainer modContainer) {
         IEventBus bus = modContainer.getEventBus();
