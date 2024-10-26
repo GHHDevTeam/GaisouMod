6f51b89ad914e31ac4a27cdf09d0b8abc606a9ee
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-10-26 14:47:53 +0900

    もでぃど

--- forge/src/main/java/io/github/ghhdevteam/gaisoumod/forge/GaisouModForge.java
+++ forge/src/main/java/io/github/ghhdevteam/gaisoumod/forge/GaisouModForge.java
@@ -6,12 +6,12 @@ import io.github.ghhdevteam.gaisoumod.mpl.MPLGaisouMod;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod(MPLGaisouMod.MOD_ID)
+@Mod(MPLGaisouMod.もでぃど)
 public class GaisouModForge {
     public GaisouModForge() {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        EventBuses.registerModEventBus(MPLGaisouMod.MOD_ID, bus);
+        EventBuses.registerModEventBus(MPLGaisouMod.もでぃど, bus);
         new MPLGaisouMod();
     }
 }
